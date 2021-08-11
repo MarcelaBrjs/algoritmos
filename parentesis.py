57f2c7b199be91bc32ee39c7ac3d3d5fd85a19e1
“MarcelaBrjs” <marcela.bg1@gmail.com>
2021-08-11 11:11:46 -0500

    Instrucciones de la actividad paréntesis

---
+++ parentesis.py
@@ -0,0 +1,13 @@
+# Actividad: Parentesis - 11/08/2021
+
+# Recibes una string conteniendo solo () y [].
+# Crea un algoritmo para validar que la sintaxis de los corchetes es correcta:
+# Ejemplos validos:
+# - '()'
+# - '()()()()()()()()()'
+# - '([])[]()'
+# Ejemplos no validos:
+# - ')'
+# - '()['
+# - '(([))]'
+# - '(((((((((((((((((((((((((((((((((('
